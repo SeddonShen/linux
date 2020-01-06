f918ffab72b60ce98a550548fbcddea1a455a0bf
ouxianfei <ouxianfei@smail.nju.edu.cn>
2020-01-06 15:54:20 +0800

    update support for mips-clock

--- arch/mips/noop/init.c
+++ arch/mips/noop/init.c
@@ -11,32 +11,28 @@
 
 #include <linux/of_fdt.h>
 #include <linux/of_platform.h>
-#include <linux/serial_8250.h>
 #include <linux/module.h>
 #include <linux/irq.h>
 #include <linux/platform_device.h>
-#include <linux/usb/sl811.h>
-#include <linux/usb/isp1362.h>
 
 #include <asm/prom.h>
 
-
-#define U_Rx     0x0
-#define U_Tx     0x4
-#define U_STAT   0x8
-#define U_CTRL   0xC
+#define U_Rx 0x0
+#define U_Tx 0x4
+#define U_STAT 0x8
+#define U_CTRL 0xC
 
 #ifndef BIT
 #define BIT(n) (1 << (n))
 #endif
 
-#define SR_TX_FIFO_FULL		BIT(3) /* transmit FIFO full */
-#define SR_TX_FIFO_EMPTY	BIT(2) /* transmit FIFO empty */
-#define SR_RX_FIFO_VALID_DATA	BIT(0) /* data in receive FIFO */
-#define SR_RX_FIFO_FULL		BIT(1) /* receive FIFO full */
+#define SR_TX_FIFO_FULL BIT(3) /* transmit FIFO full */
+#define SR_TX_FIFO_EMPTY BIT(2) /* transmit FIFO empty */
+#define SR_RX_FIFO_VALID_DATA BIT(0) /* data in receive FIFO */
+#define SR_RX_FIFO_FULL BIT(1) /* receive FIFO full */
 
-#define ULITE_CONTROL_RST_TX	0x01
-#define ULITE_CONTROL_RST_RX	0x02
+#define ULITE_CONTROL_RST_TX 0x01
+#define ULITE_CONTROL_RST_RX 0x02
 
 #define CONFIG_DEBUG_UART_BASE ((void *)0xbfe50000)
 
@@ -47,93 +43,15 @@ struct uartlite {
   unsigned int control;
 };
 
-#if IS_ENABLED(CONFIG_USB_SL811_HCD) || IS_ENABLED(CONFIG_USB_ISP1362_HCD)
-static struct resource sl811_hcd_resources[] = {
-  {
-	.start = 0x1c020000,
-	.end = 0x1c020000,
-	.flags = IORESOURCE_MEM,
-  }, {
-	.start = 0x1c020004,
-	.end = 0x1c020004,
-	.flags = IORESOURCE_MEM,
-  }, {
-#if IS_ENABLED(CONFIG_DE2I_CYCLONE4)
-	.start = 8+30,
-	.end = 8+30,
-#else
-	.start = 5,
-	.end = 5,
-#endif
-	//#if IS_ENABLED(CONFIG_USB_SL811_HCD)
-	.flags = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL,
-	//#else
-	//		.flags = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
-	//#endif
-  },
-};
-#endif
-
-#if IS_ENABLED(CONFIG_USB_SL811_HCD)
-#if defined(CONFIG_USB_SL811_BFIN_USE_VBUS)
-void sl811_port_power(struct device *dev, int is_on)
-{
-  // gpio_request(CONFIG_USB_SL811_BFIN_GPIO_VBUS, "usb:SL811_VBUS");
-  // gpio_direction_output(CONFIG_USB_SL811_BFIN_GPIO_VBUS, is_on);
-}
-#endif
-
-static struct sl811_platform_data sl811_priv = {
-  .potpg = 10,
-  .power = 250,       /* == 500mA */
-#if defined(CONFIG_USB_SL811_BFIN_USE_VBUS)
-  .port_power = &sl811_port_power,
-#endif
-};
-
-static struct platform_device sl811_hcd_device = {
-  .name = "sl811-hcd",
-  .id = 0,
-  .dev = {
-	.platform_data = &sl811_priv,
-  },
-  .num_resources = ARRAY_SIZE(sl811_hcd_resources),
-  .resource = sl811_hcd_resources,
-};
-#endif //CONFIG_USB_SL811_HCD
-
-#if IS_ENABLED(CONFIG_USB_ISP1362_HCD)
-static struct isp1362_platform_data isp1362_priv = {
-  .sel15Kres = 1,
-  .clknotstop = 0,
-  .oc_enable = 0,
-  .int_act_high = 1,
-  .int_edge_triggered = 0,
-  .remote_wakeup_connected = 0,
-  .no_power_switching = 1,
-  .power_switching_mode = 0,
-};
-
-static struct platform_device isp1362_hcd_device = {
-  .name = "isp1362-hcd",
-  .id = 0,
-  .dev = {
-	.platform_data = &isp1362_priv,
-  },
-  .num_resources = ARRAY_SIZE(sl811_hcd_resources),
-  .resource = sl811_hcd_resources,
-};
-#endif
-
 const char *get_system_type(void)
 {
-  return "NOOP";
+  return "noop";
 }
 
 void __init plat_mem_setup(void)
 {
   __dt_setup_arch(__dtb_start);
-  strlcpy(arcs_cmdline, boot_command_line, COMMAND_LINE_SIZE);
+  // strlcpy(arcs_cmdline, boot_command_line, COMMAND_LINE_SIZE);
 }
 
 void prom_putchar(char ch)
@@ -141,7 +59,7 @@ void prom_putchar(char ch)
   struct uartlite *regs = (struct uartlite *)CONFIG_DEBUG_UART_BASE;
 
   while (readl(&regs->status) & SR_TX_FIFO_FULL)
-	;
+    ;
 
   writel(ch & 0xff, &regs->tx_fifo);
 }
@@ -162,28 +80,14 @@ void __init prom_free_prom_memory(void)
 void __init device_tree_init(void)
 {
   if (!initial_boot_params)
-	return;
+    return;
 
   unflatten_and_copy_device_tree();
 }
 
 static int __init plat_of_setup(void)
 {
-  /*
-  if (!of_have_populated_dt())
-	panic("Device tree not present");
-
-  if (of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL))
-	panic("Failed to populate DT");
-
-#ifdef CONFIG_USB_SL811_HCD
-  platform_device_register(&sl811_hcd_device);
-#endif
-#if IS_ENABLED(CONFIG_USB_ISP1362_HCD)
-  platform_device_register(&isp1362_hcd_device);
-#endif
-  */
-
   return 0;
 }
+
 arch_initcall(plat_of_setup);
--- arch/mips/noop/time.c
+++ arch/mips/noop/time.c
@@ -18,4 +18,25 @@
 
 void __init plat_time_init(void)
 {
+	struct device_node *np;
+	struct clk *clk;
+
+	of_clk_init(NULL);
+	// clocksource_probe();
+
+	np = of_get_cpu_node(0, NULL);
+	if (!np) {
+		pr_err("Failed to get CPU node\n");
+		return;
+	}
+
+	clk = of_clk_get(np, 0);
+	if (IS_ERR(clk)) {
+		pr_err("Failed to get CPU clock: %ld\n", PTR_ERR(clk));
+		return;
+	}
+
+	mips_hpt_frequency = clk_get_rate(clk);
+	printk("mips_hpt_frequency: %d HZ\n", mips_hpt_frequency);
+	clk_put(clk);
 }
